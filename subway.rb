a61e346ce07438ae4638ba6025082268f7dcb2b1
rehabsaadoun <YOUR GITHUB EMAIL>
2019-04-09 07:32:10 +0300

    HW

--- subway.rb
+++ subway.rb
@@ -5,13 +5,15 @@ SUBWAY_LINES{
 }
 travel_path=[]
 # def get_path(on_line, on_stop,off_line_off_stop)
-i=subway_lines[on_line].index(on_stop)+1
+i=subwaysubway_lines_lines[on_line].index(on_stop)+1
 
 while i<subway_lines[off_line].index(off_stop)
-    travel_path<<subway_lines_on_line.index(i)
+     travel_path<<subway_lines_on_line.index(i)
 def get_total_stops
 
-
+    subway_lines = " N line, L line, 6 Line"
+ total_stop = 0
+ 
 # planTrip("N", "Times Square", "6", "33rd"); 
 # plan_trip( :n, "Times Square", :l, "Grand Central" )
 # This is only a suggested function name and signature.
@@ -22,29 +24,14 @@ def get_total_stops
 # "Your journey continues through the following stops: 23rd, 28th, 33rd."
 #  "7 stops in total."
 
-@on_line= "N line,L line,6 line".split("")
-puts " N line, L line, 6 Line"
-on_line = gets.chomp.downcase
-end
+def plan_trip ("N line, L line, 6 Line)
+    if on_line == off_line
+   puts  "You must travel through the following stops on the N line: 34th, 28th, 23rd, Union Square." 
+   puts "Change at Union Square." 
 
-
-@on_stop"N line,L line,6 line".split("")
-puts " N line, L line, 6 Line"
-on_stop = gets.chomp.downcase
-end
-
-@off_line= "N line,L line,6 line".split("")
-puts " N line, L line, 6 Line"
-off_line= gets.chomp.downcase
-end
-
-@off_stop= "N line,L line,6 line".split("")
-puts " N line, L line, 6 Line"
-off_stop= gets.chomp.downcase
-end
 
 " Union Square".include?" N line, L line, 6 Line"
-puts true
+
 
 
 

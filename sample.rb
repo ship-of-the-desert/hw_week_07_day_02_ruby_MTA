17b7ee4f3914f9953fe44ec23dfe06f91cdfcace
Reema Al-Rayes <reema@Reemas-MacBook-Pro.local>
2019-04-09 08:53:34 +0300

    hw_w7_d2

---
+++ sample.rb
@@ -0,0 +1,27 @@
+lineN = ["Times Square", "34th", "28th_nline", "23rd", "Union Square", "8th"];
+lineL = ["8th", "6th", "Union Square", "3rd", "1st"]
+lineSix = ["Grand Central", "33rd", "28th_6line", "23rd", "Union Square", "Astor Place"]
+# all 3 lines intrsect at union square
+
+plan_trip( :n, "Times Square", :l, "Grand Central" )
+
+break if reach union square
+count the stops using
+
+
+# loop do 
+# puts lineN
+# break if lineN == "Union Square"
+# end
+# puts lineN
+
+# def pickup ()
+# def dropoff ()
+    # if the user started from stop, it will continue to other stops (from other arrays) until reach dropoff 
+    # it will change from stop to another by taking the intersection between them (eg. union square)
+    # it will print the number of stops
+    
+
+puts "the number of stops:" + lineN.size #or use length
+
+
